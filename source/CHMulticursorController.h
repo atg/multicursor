dda6896fd83ac905a797accc9ce1965ffd7ca9cc
Alex Gordon <anything@fileability.net>
2011-10-07 17:00:24 +0200

    Edited source/CHMulticursorController.h via GitHub

--- source/CHMulticursorController.h
+++ source/CHMulticursorController.h
@@ -9,6 +9,9 @@
 - (NSLayoutManager *)layoutManager;
 - (void)performTextViewActionInContext:(SEL)action argument:(id)argument; // Perform a text view action (select word right, etc) multiple times in the mulctx
 
+- (void)insertText:(NSString *)text view:(NSTextView *)view;
+- (void)textDidChangeInRange:(NSRange)range newLength:(NSInteger)newLength;
+
 - (BOOL)handleEvent:(NSEvent *)event; // Return NO if the event was not handled
 
 @end
