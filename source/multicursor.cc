2e3a175e550a492b5a5a5eec5be243edae91b88a
Ricardo Mendes <rokusu@gmail.com>
2011-10-08 18:07:27 +0100

    changes to the source code.

--- source/multicursor.cc
+++ source/multicursor.cc
@@ -55,6 +55,7 @@ range range::truncateToFitLength(int64_t documentLength) {
 bool range::indexesIntersect(range rng) {
 	if (rng.length == 0 || length == 0)
 		return false;
+
 	return rng.hasIndex(lastIndex()) || rng.hasIndex(location);
 }
 
@@ -62,7 +63,7 @@ bool range::indexesIntersect(range rng) {
 // context
 //
 bool context::isCoherent() {
-  
+  return ranges.size == 1 || focused_range_index != -1
 }
 
 bool context::isValid() {
@@ -78,24 +79,38 @@ int64_t context::indexForRange(range rng) {
 }
 
 range context::primaryRange() {
-  return (focused_range_index) < 0 ? ranges[0] : ranges[focused_range_index];
+  return focused_range_index < 0 ? ranges[0] : ranges[focused_range_index];
 }
 
 bool context::addRange(int64_t rng) {
-
+  ranges.push_back(rng);
+  
+  return true;
 }
 bool context::deleteRange(range rng) {
   
+  return true;
 }
 bool context::deleteRanges(std::vector<range> rngs) {
-  
+  bool flag = false;
+  for (std::vector<range> it = rgns.begin() , et = rgns.end() ; it != et ; ++it)
+    flag = true && deleteRange(*it);
+    
+  return flag;
 }
 
 bool context::focus(range rng) {
+  for ( std::vector<range>::iterator it = ranges.begin() , et = ranges.end() ; it != et ; ++it)
+      if (it == rng) { focused_range_index = it - ranges.begin()
+                     ; return true }
   
+  return false;
 }
 bool context::unfocus() {
+  if (focused_range_index == -1) return false;
+
   focused_range_index = -1;
+  return true;
 }
 
 bool context::performAction(range (^callback)(range rng, bool* shouldDelete)) {
